28167ca02865b19382f7c8abd2e126ecc3c0fd30
slave <syedamanals86@gmail.com>
2026-02-04 10:30:57 +0300

    day3

---
+++ src/day3_collections.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  4 08:41:03 2026
+
+@author: Lenovo
+"""
+
+# day 3 list and tuples
+# create a list - add to it - slice element - store it in new list - print it
+
+class_details = ["learning center",2,]
+print(class_details)
+
+
+
+coordinates = (12.3, 22.5)
+print(coordinates)
+
+# use a student data - change it by index - add to it - sort it - and print it
+
+# task 1
+inventory = ["Apple","Banana", "Carrots", "Dates" ]
+print(inventory)
+inventory.append("Eggs")
+print(inventory)
+inventory.remove("Banana")
+print(inventory)
+
+#task 2
+temprature = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
