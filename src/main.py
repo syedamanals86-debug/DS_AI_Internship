3144e795dc0923fa1bdb6fd8d769aa2d9e9dd1e6
slave <syedamanals86@gmail.com>
2026-02-06 13:45:17 +0300

    day5 main

--- src/main.py
+++ src/main.py
@@ -5,3 +5,11 @@ Created on Fri Feb  6 12:46:19 2026
 @author: Lenovo
 """
 
+import  math_operations 
+
+power = math_operations.power(2,10)
+print("2^10 =", power)
+
+numbers = [10, 20, 30, 40]
+average = math_operations.average(numbers)
+print("Average =", average)
